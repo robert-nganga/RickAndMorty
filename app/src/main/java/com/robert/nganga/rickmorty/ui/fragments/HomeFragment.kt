19ff85c9ce163076aba32d64f874d6c29c97368e
robert-nganga <robertnganga220@gmail.com>
2023-02-11 13:38:07 +0300

    UI improvements

--- app/src/main/java/com/robert/nganga/rickmorty/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/robert/nganga/rickmorty/ui/fragments/HomeFragment.kt
@@ -6,7 +6,6 @@ import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
@@ -14,8 +13,6 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.paging.LoadState
-import androidx.paging.LoadStateAdapter
-import androidx.recyclerview.widget.ConcatAdapter
 import androidx.recyclerview.widget.GridLayoutManager
 import com.robert.nganga.rickmorty.R
 import com.robert.nganga.rickmorty.adapters.CharactersAdapter
@@ -24,7 +21,6 @@ import com.robert.nganga.rickmorty.adapters.CharactersLoadStateAdapter
 import com.robert.nganga.rickmorty.databinding.FragmentHomeBinding
 import com.robert.nganga.rickmorty.ui.MainActivity
 import com.robert.nganga.rickmorty.ui.RickMortyViewModel
-import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
 
@@ -46,7 +42,7 @@ class HomeFragment: Fragment(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as MainActivity).viewModel
-        adapter = CharactersAdapter(getDeviceWidth())
+        adapter = CharactersAdapter(getImageWidth())
         binding.bindAdapter(adapter)
         adapter.setOnItemClickListener {
             val bundle = Bundle().apply {
@@ -55,18 +51,21 @@ class HomeFragment: Fragment(R.layout.fragment_home) {
             findNavController().navigate(R.id.action_homeFragment_to_characterDetailsFragment, bundle)
         }
 
-        //binding.retryButton.setOnClickListener { adapter.retry() }
-        lifecycleScope.launch {
+        binding.retryButton.setOnClickListener { adapter.retry() }
+        viewLifecycleOwner.lifecycleScope.launch {
             adapter.loadStateFlow.collect { loadState ->
                 val isListEmpty = loadState.refresh is LoadState.Error && adapter.itemCount == 0
                 // show empty list
                 binding.emptyList.isVisible = isListEmpty
+
                 // Only show the list if refresh succeeds.
                 binding.rvCharacters.isVisible = !isListEmpty
+
                 // Show loading spinner during initial load or refresh.
-                binding.progressBar.isVisible = loadState.refresh is LoadState.Loading
+                binding.progressBar.isVisible = loadState.refresh is LoadState.Loading && adapter.itemCount == 0
+
                 // Show the retry state if initial load or refresh fails.
-                //retryButton.isVisible = loadState.source.refresh is LoadState.Error
+                binding.retryButton.isVisible = loadState.refresh is LoadState.Error && adapter.itemCount == 0
 
                 // Toast on any error, regardless of whether it came from RemoteMediator or PagingSource
 //                val errorState = loadState.source.append as? LoadState.Error
@@ -83,7 +82,7 @@ class HomeFragment: Fragment(R.layout.fragment_home) {
             }
         }
 
-        lifecycleScope.launch {
+        viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED){
                 viewModel.characters.collect{
                     adapter.submitData(it)
@@ -99,12 +98,12 @@ class HomeFragment: Fragment(R.layout.fragment_home) {
     }
 
     private fun FragmentHomeBinding.bindAdapter(charactersAdapter: CharactersAdapter){
-        //rvCharacters.adapter = charactersAdapter
-        val gridLayoutManager = GridLayoutManager(requireContext(), 2)
+        val span = if (getDeviceWidth() > 700) 3 else 2
+        val gridLayoutManager = GridLayoutManager(requireContext(), span, GridLayoutManager.VERTICAL, false)
         gridLayoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
             override fun getSpanSize(position: Int): Int {
                 return if (charactersAdapter.getItemViewType(position) == LOADING_ITEM)
-                    1 else 2
+                    1 else span
             }
         }
         rvCharacters.layoutManager = gridLayoutManager//GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
@@ -113,14 +112,25 @@ class HomeFragment: Fragment(R.layout.fragment_home) {
         )
     }
 
-    private fun getDeviceWidth(): Int {
-        val displayMetrics = resources.displayMetrics
-        val paddingWidthInDp = 26 // width in dp
+    private fun getImageWidth(): Int {
+        val deviceWidth = getDeviceWidth()
+        val paddingWidthInDp = 26 // padding in dp
         val paddingWidthInPx = TypedValue.applyDimension(
             TypedValue.COMPLEX_UNIT_DIP,
             paddingWidthInDp.toFloat(), resources.displayMetrics).toInt()
+        val width = deviceWidth - paddingWidthInPx
+        return if (width > 700) {
+            width / 3
+        } else {
+            width / 2
+        }
+        //return displayMetrics.widthPixels - paddingWidthInPx
+    }
+
+    private fun getDeviceWidth(): Int {
+        val displayMetrics = resources.displayMetrics
         Log.i("HomeFragment", displayMetrics.widthPixels.toString())
-        return displayMetrics.widthPixels - paddingWidthInPx
+        return displayMetrics.widthPixels
     }
 
 }
