65af86d7045906577f0e8f2dbb2b286a1f66bfc5
robert-nganga <robertnganga220@gmail.com>
2023-02-13 10:39:28 +0300

    added apk and screenshots

--- app/src/main/java/com/robert/nganga/rickmorty/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/robert/nganga/rickmorty/ui/fragments/HomeFragment.kt
@@ -1,7 +1,6 @@
 package com.robert.nganga.rickmorty.ui.fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
